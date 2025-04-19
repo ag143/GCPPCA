903e3f4b1c5404124c8978ade0f8190bbb7401d5
ag143 <ag14341@gmail.com>
2025-04-19 07:07:16 +0530

    Create clone.sh

---
+++ clone.sh
@@ -0,0 +1,29 @@
+git clone https://github.com/githubtraining/training-manual.git
+sleep 60
+git clone https://github.com/argoproj/argo-cd.git
+sleep 60
+git clone https://github.com/hashicorp/terraform.git
+sleep 60
+git clone https://github.com/kubernetes/website.git
+sleep 60
+git clone https://github.com/priyankavergadia/GCPSketchnote.git
+sleep 60
+git clone https://github.com/mikeroyal/Google-Cloud-Guide.git
+sleep 60
+git clone https://github.com/Patechoc/GCP_memo.git
+sleep 60
+git clone https://github.com/larrybotha/gcp-fundamentals.git
+sleep 60
+git clone https://github.com/CodeWithHarry/The-Ultimate-Python-Course.git
+sleep 60
+git clone https://github.com/Asabeneh/30-Days-Of-Python.git
+sleep 60
+git clone https://github.com/newrelic/docs-website.git
+sleep 60
+git clone https://github.com/hashicorp/vault-guides.git
+sleep 60
+git clone https://github.com/naveensilver/Ansible.git
+sleep 60
+git clone https://github.com/fleschutz/PowerShell.git
+sleep 60
+git clone https://github.com/ag143/GCPPCA.git
